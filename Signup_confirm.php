b166618d067ccfd89f35802c8ed350df1cb36ec5
kc2585 <52477052+kc2585@users.noreply.github.com>
2019-07-31 15:29:35 +0900

    Create Signup_confirm.php

---
+++ Signup_confirm.php
@@ -0,0 +1,17 @@
+<?php
+	include "../db.php";
+	include "../password.php";
+
+	$userid = $_POST['userid'];
+	$userpw = password_hash($_POST['userpw'], PASSWORD_DEFAULT);
+	$username = $_POST['name'];
+	$adress = $_POST['adress'];
+	$sex = $_POST['sex'];
+	$email = $_POST['email'].'@'.$_POST['emadress'];
+
+$sql = mq("insert into member (id,pw,name,adress,sex,email) values('".$userid."','".$userpw."','".$username."','".$adress."','".$sex."','".$email."')");
+
+?>
+<meta charset="utf-8" />
+<script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
+<meta http-equiv="refresh" content="0 url=/">
