40df9d32578604817af2d32f51e6d4f6c139224c
kc2585 <52477052+kc2585@users.noreply.github.com>
2019-08-20 13:38:20 +0900

    Create distance.ino

---
+++ distance.ino
@@ -0,0 +1,52 @@
+/*
+  Measuring Arduino Distance Using Infrared Sensors
+                                                   */
+
+const int ledPin = 13;
+const int sensorPin = 0;
+
+const long referenceMv = 5000;
+
+void setup()
+{
+  Serial.begin(9600);
+  PinMode (ledPin, OUTPUT);
+}
+
+void loop()
+{
+  int val = analogRead(sensorPin);
+  int mV = (val * referenceMv) / 1023;
+  
+  Serial.print(mV);
+  Serial.print(",");
+  int cm = getDistance(mV);
+  Serial.println(cm);
+  
+  digitalWrite(ledPin, HIGH);
+  delay(cm * 10);
+  digitalWrite(ledPin, LOW);
+  delay (cm * 10);
+  
+  delay(100);
+}
+
+// next code is used to adjust the distance of the table
+// table items are calculated based on 250 millivolts
+
+const int TABLE_ENTRIES = 12;
+const int firstElement = 250;
+const int INTERVAL = 250;
+static int distance[TABLE_ENTRIES] = {150, 140, 130, 100, 60, 50, 40, 35, 30, 25, 20, 15};
+
+int getDistance(int mV)
+{
+  if(mV > INTERVAL * TABLE_ENTRIES-1)
+    return distance[TABLE_ENTRIES-1];
+  else
+  {
+    int index = mV / INTERVAL;
+    float frac = (mV % 250) / (float)INTERVAL;
+    return distance[index] - ((distance[index] - distance[index+1]) * frac);
+  }
+}
