9defaa06e9db517dbbc75bf0142e6fbff5ef7f87
kkccmm1202 <52639963+kkccmm1202@users.noreply.github.com>
2019-09-10 14:08:12 +0900

    Add files via upload

--- main/java/com/example/ex/MainActivity.java
+++ main/java/com/example/ex/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
